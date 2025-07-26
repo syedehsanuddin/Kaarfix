918e949ba6a6392f9d4d1b113fd7b1e8ce95dd73
ahsan.uddin <ahsan.uddin@nielseniq.com>
2025-07-26 16:31:00 +0500

    Logo change

--- .next/static/rN0YfD31H3Vk2t_VM7Gm3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/399-83a17d2cff27598e.js","static/chunks/pages/index-81a237c2f175ddd0.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
